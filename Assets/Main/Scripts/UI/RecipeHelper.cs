77a247b4767c8e80ec15efd32a9fdd37c344abc3
madeflyoke <sano555000@gmail.com>
2024-03-17 22:57:15 +0300

    feat: ui

--- Assets/Main/Scripts/UI/RecipeHelper.cs
+++ Assets/Main/Scripts/UI/RecipeHelper.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Main.Scripts.Flowers;
+using Main.Scripts.Input;
 using UnityEngine;
 using Zenject;
 
@@ -9,10 +11,27 @@ namespace Main.Scripts.UI
     public class RecipeHelper : MonoBehaviour
     {
         [Inject] private FlowerRecipeSetup _flowerRecipeSetup;
+        [Inject] private InputHandler _inputHandler;
+        [SerializeField] private GameObject _recepiesWindow;
         private HashSet<IngredientRatio> _currentRecipe;
         private FlowerType _currentFlower;
         private StringBuilder _recipeDesc;
-        
+        private bool _isActive;
+
+        private void Start()
+        {
+            _inputHandler.OnKeyboardButtonClick += Show;
+            _inputHandler.OnKeyboardButtonClick += Hide;
+            _recepiesWindow.gameObject.SetActive(false);
+        }
+
+        private void OnDisable()
+        {
+            _inputHandler.OnKeyboardButtonClick -= Show;
+            _inputHandler.OnKeyboardButtonClick -= Hide;
+        }
+
+
         public void SetHelperByFlowerType(FlowerType type)
         {
             _currentFlower = type;
@@ -24,14 +43,22 @@ namespace Main.Scripts.UI
             }
         }
         
-        public void Show()
+        public void Show(ButtonType buttonType)
         {
-            
+            if (buttonType==ButtonType.PotionGuide)
+            {
+                _isActive = true;
+                _recepiesWindow.gameObject.SetActive(true);
+            }
         }
         
-        public void Hide()
+        public void Hide(ButtonType buttonType)
         {
-            
+            if (buttonType==ButtonType.PotionGuide)
+            {
+                _isActive = false;
+                _recepiesWindow.gameObject.SetActive(false);
+            }
         }
     }
 }
