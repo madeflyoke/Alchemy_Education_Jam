113bc92f1a128935d268abefa7f462e9347080f8
madeflyoke <sano555000@gmail.com>
2024-04-05 11:24:00 +0300

    fix: hand

--- Assets/Main/Scripts/Hand/HandGrabberSwitcher.cs
+++ Assets/Main/Scripts/Hand/HandGrabberSwitcher.cs
@@ -1,37 +1,43 @@
+using System;
 using EasyButtons;
 using Main.Scripts.Audio;
+using Main.Scripts.Input;
 using UnityEngine;
+using Zenject;
 
 namespace Main.Scripts.Hand
 {
    public class HandGrabberSwitcher : MonoBehaviour
    {
+      [Inject] private InputHandler _inputHandler;
+
+      
       [SerializeField] private GameObject _grabbedHand;
       [SerializeField] private GameObject _releasedHand;
 
-      private void Awake()
+      private void OnEnable()
+      {
+         _inputHandler.SubscribeOnInputEvent(KeysEventType.MousePrimaryDown, SetGrabbed);
+         _inputHandler.SubscribeOnInputEvent(KeysEventType.MousePrimaryUp, SetReleased);
+         SetReleased();
+      }
+
+      private void OnDisable()
+      {
+         _inputHandler.UnsubscribeFromInputEvent(KeysEventType.MousePrimaryDown, SetGrabbed);
+         _inputHandler.UnsubscribeFromInputEvent(KeysEventType.MousePrimaryUp, SetReleased);
+      }
+
+      private void SetGrabbed()
+      {
+         _grabbedHand.SetActive(true);
+         _releasedHand.SetActive(false);
+      }
+
+      private void SetReleased()
       {
          _grabbedHand.SetActive(false);
          _releasedHand.SetActive(true);
-      }
-
-      private void Update()
-      {
-         if (UnityEngine.Input.GetKeyDown(KeyCode.Mouse0))
-         {
-            Grab(true);
-         }
-         else if(UnityEngine.Input.GetKeyUp(KeyCode.Mouse0))
-         {
-            Grab(false);
-         }
-      }
-
-      [Button]
-      public void Grab(bool isGrabbed)
-      {
-         _grabbedHand.SetActive(isGrabbed);
-         _releasedHand.SetActive(!isGrabbed);
       }
    }
 }
